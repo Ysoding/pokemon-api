2a06e359a8de170cd0497f06031f589027d3531f
ultraman <1394466835@qq.com>
2024-07-05 21:28:14 +0800

    first commit

---
+++ src/test/kotlin/com/github/ysoding/pokemonapi/PokemonApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.ysoding.pokemonapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokemonApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
